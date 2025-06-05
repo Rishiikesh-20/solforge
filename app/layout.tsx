04c420347121016395113d3d8c4cdc8c9126dd23
Rishiikesh <rishiikeshsk@gmail.com>
2025-06-05 23:55:25 +0530

    feat : add getMetadata logic and some ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WalletContextProvider } from "@/components/WalletContextProvider";
+import { NavBar } from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-900 min-h-screen`}
       >
-        <WalletContextProvider>
-          {children}
-        </WalletContextProvider>
+        <div className="flex flex-col items-center top-1 z-50 ">
+           <WalletContextProvider>
+            <NavBar />
+            {children}
+          </WalletContextProvider>
+        </div>
       </body>
     </html>
   );
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="text-lg text-white">Loading...</span>
+    </div>
+  );
+}
