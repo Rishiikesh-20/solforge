a0fbaf25ae17c161cd6ea5418e355df85c5861b6
Rishiikesh <rishiikeshsk@gmail.com>
2025-06-06 01:04:11 +0530

    feat: add navbar and search ui component

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -5,7 +5,7 @@ type searchInputProps=InputHTMLAttributes<HTMLInputElement>
 export function SearchInput({...props}:searchInputProps){
     return(
         <div className="">
-            <input className={`border-1 px-4 py-2 min-w-[350px] rounded-lg border-green-700 focus:border-cyan-400 focus:border-2 outline-none text-slate-50`} {...props} />
+            <input className={`w-full px-4 py-2 rounded-lg border border-green-600 bg-slate-800 text-slate-100 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-cyan-400 transition duration-200 shadow-sm`} {...props} />
         </div>
     )
 }
